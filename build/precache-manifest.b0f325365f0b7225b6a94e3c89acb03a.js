042276e0355e4c8d91efca0c644bc170c5abb85e
Jyotsnalal <das.jyotsna13@gmail.com>
2024-08-18 00:21:21 +0530

    first

---
+++ build/precache-manifest.b0f325365f0b7225b6a94e3c89acb03a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d9a7c1b6967f26e0c7c1efed6bde64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67240078d8b152e09530",
+    "url": "/static/css/main.e1861483.chunk.css"
+  },
+  {
+    "revision": "41fe157a2ad6c9eea8ca",
+    "url": "/static/js/2.41d88a95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41d88a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67240078d8b152e09530",
+    "url": "/static/js/main.9685a3a7.chunk.js"
+  },
+  {
+    "revision": "58a120207ea832ae23c1",
+    "url": "/static/js/runtime-main.242776d8.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/logo.ed28accb.svg"
+  }
+]);
